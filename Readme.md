417365eaa73d4ab405e611c7f61e00a26581885f
Filip <flipchan@riseup.net>
2023-10-23 22:13:32 +0200

    base

---
+++ Readme.md
@@ -0,0 +1,35 @@
+## XCMSend Json Api
+
+
+### Build:  
+```
+npm run install
+```
+
+### Run:   
+```
+npm run api  
+```
+
+
+### Info:  
+
+##### `/polkadot/xcm-asset-transfer`:
+
+
+##### `/polkadot/xcm-native-transfer`:
+
+##### `/polkadot/openchannels`:
+
+##### `/call/template`:
+
+##### `/call/scenario`:
+
+
+##### `//xcm-native-transfer`:
+
+
+### Code example:  
+
+
+
--- app.ts
+++ app.ts
@@ -1,13 +1,61 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 8080;
 
-// Define a sample route
-app.get('/api/data', (req, res) => {
-  const jsonData = { message: 'Hello, JSON API!' };
-  res.json(jsonData);
+// Use body-parser middleware to parse JSON
+app.use(bodyParser.json());
+
+// open channels, list open ingoing and outgoing hrmp channels for paraid
+app.post('/polkadot/openchannels', (req, res) => {
+  const jsonData = req.body.paraid; // get the chains paraid
+  res.json({ receivedData: jsonData });
 });
+
+// create scenerio 
+
+
+// Define a POST route to handle JSON data
+app.post('/api/data', (req, res) => {
+  const jsonData = req.body;
+  res.json({ receivedData: jsonData });
+});
+
+
+
+// tx broadcast
+
+// xcm asset transfer
+// call a scenerio - call a scenario you created in the UI - todo
+app.post('/polkadot/xcm-asset-transfer', (req, res) => {
+  const jsonData = req.body.scenarioid;
+  res.json({ receivedData: "todo" });
+});
+
+app.post('/polkadot/xcm-native-transfer', (req, res) => {
+  const jsonData = req.body.scenarioid;
+  res.json({ receivedData: "todo" });
+});
+
+
+
+
+// use template - todo
+app.post('/call/template', (req, res) => {
+  const jsonData = req.body.scenarioid;
+  res.json({ receivedData: "todo" });
+});
+
+
+
+// call a scenerio - call a scenario you created in the UI - todo
+app.post('/call/scenerio', (req, res) => {
+  const jsonData = req.body.scenarioid;
+  res.json({ receivedData: "todo" });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
